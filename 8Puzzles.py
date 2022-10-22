3162a678bfa96be64e1c461c535ce87fe5d52277
Yuteng Terrence Wu <yutengwu@yahoo.com>
2022-10-22 13:08:12 -0700

    Update 8Puzzles.py
    lunch break

--- 8Puzzles.py
+++ 8Puzzles.py
@@ -1,17 +1,48 @@
 from pip import main
 
 class PuzzelGame:
-    def __init__(self, firstRow, secondRow, thirdRow):
-        self.firstRow = firstRow
-        self.secondRow = secondRow
-        self.thirdRow = thirdRow
+    """ keep the puzzel  in one dimension array """
+    def __init__(self, initialState):
+        self.puzzelBoard = initialState
+        self.zeroPosition = 0;
+        self.gameSize = len(self.puzzelBoard);
+        
 
 
     def printGame(self):
-        print(self.firstRow)
-        print(self.secondRow)
-        print(self.thirdRow)
-        print("/n")
+        for i in range(len(self.puzzelBoard)):
+            print(self.puzzelBoard[i])
+            if(i == 2 or i == 5 or i == 8):
+                print("\n")
+    
+    def uniformCostSearch(self):
+        """ Uniform Cost Search """
+
+
+    def findZeroPos(self):
+        for i in range(len(self.puzzelBoard)):
+            if(i == 0):
+                self.zeroPos = i
+
+    """need to use mod instead fixed number since we need to make the game interchangeble to different size"""
+    def moveUp(self):
+        if self.zeroPos != 0 or self.zeroPos != 1 or self.zeroPos != 2:
+            temp = self.puzzelBoard[self.zeroPos - 3]
+            self.puzzelBoard[self.zeroPos - 3] = self.puzzelBoard[self.zeroPos]
+            self.puzzelBoard[self.zeroPos] = temp
+            self.zeroPos = self.zeroPos - 3
+    
+    def moveDown(self):
+        if self.zeroPos != 6 or self.zeroPos != 7 or self.zeroPos != 8:
+            temp = self.puzzelBoard[self.zeroPos + 3]
+            self.puzzelBoard[self.zeroPos + 3] = self.puzzelBoard[self.zeroPos]
+            self.puzzelBoard[self.zeroPos] = temp
+            self.zeroPos = self.zeroPos + 3
+    def moveLeft(self):
+
+
+    def moveRight(self):
+    
 
 class PuzzelGameIntro:
     def __init__(self) -> None:
@@ -20,16 +51,30 @@ class PuzzelGameIntro:
     def intro(self):
         print("Welcome to the 8 Puzzel Game Solver Program")
         userInput = input("Type \"1\" to start with a default puzzle Type \"2\" to start with your own custom puzzle: ")
-        
+        puzzelBoard = []
         if userInput == "2":
             print("Please enter your puzzle below, use a zero to represent the blank")
             print("Enter the first row, use space or tabs between numbers")
             firstRow = input()
+            puzzelBoard.append(firstRow)
             print("Enter the second row, use space or tabs between numbers")
             secondRow = input()
+            puzzelBoard.append(secondRow)
             print("Enter the third row, use space or tabs between numbers")
             thirdRow = input()
-            return PuzzelGame(firstRow, secondRow, thirdRow)
+            puzzelBoard.append(thirdRow)
+
+            print("Please select the algorithm you would like to use: ")
+            print("(1). Uniform Cost Search / Breadth First Search")
+            print("(2). A* with Misplaced Tile Heuristic")
+            print("(3). A* with Manhattan Distance Heuristic")
+            userChoice = input()
+            print(userChoice)
+
+            return PuzzelGame(puzzelBoard)
+    
+    
+        
     
     
         
@@ -37,9 +82,11 @@ class PuzzelGameIntro:
 
     
 def main():
-    game = PuzzelGameIntro()
+    start = PuzzelGameIntro()
 
-    puzzel = game.intro()
-    puzzel.printGame()
+    game = start.intro()
+
+    game.printGame()
+    
 
 main()
