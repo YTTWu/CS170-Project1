bab6ba475d7e400bfc298385907ca5292bc930dc
Yuteng Terrence Wu <yutengwu@yahoo.com>
2022-10-23 17:08:21 -0700

    Update 8Puzzles.py
    Algorithm 1 is 95% done. Need to come back to fix some numbers so that it will be easy to use for different sizes of the game.  Refer to the comments.

--- 8Puzzles.py
+++ 8Puzzles.py
@@ -2,12 +2,24 @@ from pip import main
 from collections import deque
 
 class Node:
-    def __init__(self, data, parent, depth, cost):
+    """
+    Define
+    data is the array that holds the puzzle
+    parent is the previous node
+    depth is the depth of the node
+    cost is the move of the node
+    """
+    def __init__(self, data, parent, depth):
         self.data = data
         self.parent = parent
         self.depth = depth
-        self.cost = cost
 
+"""
+puzzelBoard is the array that holds the puzzle values
+zeroPos will hold the position of the zero in the array
+findZeroPos() will find the position of the zero in the array
+gameSize is the size of the game ex. 3x3, 4x4, 5x5
+"""
 class PuzzelGame:
     """ keep the puzzel in one dimension array """
     def __init__(self, initialState):
@@ -16,67 +28,114 @@ class PuzzelGame:
         self.findZeroPos()
         self.gameSize = len(self.puzzelBoard);
         
+    """
+    print the game, since its in an array, make a new line when the index is 2, 5, 8
+    """
     def printGame(self):
+        print("--------------------------------- NEW MOVE ---------------------------------")
         for i in range(len(self.puzzelBoard)):
             print(self.puzzelBoard[i], end = " ")
             if(i == 2 or i == 5 or i == 8):
                 print("\n")
 
+    def printPath(self, parent):
+        for i in range(len(self.puzzelBoard)):
+            print(self.puzzelBoard[i], end = " ")
+            if(i == 2 or i == 5 or i == 8):
+                print("\n")
+        if parent:
+            print("PREVIOUS LEVEL")
+            print("               |")
+            print("               |")
+            print("               V\n")
 
-
+    """ getter for the array value of the puzzel """
     def getBoard(self):
         return self.puzzelBoard
 
 
     """ Uniform Cost Search """
     def uniformCostSearch(self):
-        root = Node(PuzzelGame(self.puzzelBoard), None, 0, 0)
+        # root node
+        root = Node(PuzzelGame(self.puzzelBoard), None, 0)
+        # object for checking the goal state
         goalCheck = GoalCheck()
         
+        # edge case
         if not root: return None
+        # starting our dequeue with the root node
         q = deque([root])
-        level = 0
+        # instantite a list to hold the visited nodes
         visited = []
+        # temp to hold the value of the next move
         temp = []
 
         
 
         while q:
+            # current state of the game move
             cur_node = q.popleft()
-
+            # get a copy of the current game state, just the value of the array
             temp = cur_node.data.getBoard().copy()
-
+            # skip if we have visited the move
             if temp in visited: continue
+            #if not add it to the visited list
             visited.append(temp)
 
-            
-
+            print("CURRENT LEVEL")
+            print("DEPTH: ", cur_node.depth)
             cur_node.data.printGame()
             
+            # check if the "0" can move up
             if cur_node.data.moveUp():
+                # get a copy of the current game state, just the value of the array
                 temp = cur_node.data.getBoard().copy()
+                # move it back to the previous position because we were just checking if we could move it
                 cur_node.data.moveDown()
+                # check if the move is repeated
                 if temp not in visited:
-                    q.append(Node(PuzzelGame(temp),cur_node, cur_node.depth+1, cur_node.cost+1))
-                
+                    # if the move is valid, add it to the queue
+                    q.append(Node(PuzzelGame(temp), cur_node, cur_node.depth+1))
+                    print("NEXT LEVEL")
+                    print("DEPTH: ", q[-1].depth)
+                    q[-1].data.printGame()
+                    # check if the move is the goal state
+                    if goalCheck.goalChecker(temp):
+                        return q.pop()
+
+            # same logic but in different direction   
             if cur_node.data.moveDown():
                 temp = cur_node.data.getBoard().copy()
                 cur_node.data.moveUp()
                 if temp not in visited:
-                    q.append(Node(PuzzelGame(temp),cur_node, cur_node.depth+1, cur_node.cost+1))
+                    q.append(Node(PuzzelGame(temp), cur_node, cur_node.depth+1))
+                    print("NEXT LEVEL")
+                    print("DEPTH: ", q[-1].depth)
+                    q[-1].data.printGame()
+                    if goalCheck.goalChecker(temp):
+                        return q.pop()
                 
-
+            # same logic but in different direction
             if cur_node.data.moveLeft():
                 temp = cur_node.data.getBoard().copy()
                 cur_node.data.moveRight()
                 if temp not in visited:
-                    q.append(Node(PuzzelGame(temp),cur_node, cur_node.depth+1, cur_node.cost+1))
+                    q.append(Node(PuzzelGame(temp), cur_node, cur_node.depth+1))
+                    print("NEXT LEVEL")
+                    print("DEPTH: ", q[-1].depth)
+                    q[-1].data.printGame()
+                    if goalCheck.goalChecker(temp):
+                        return q.pop()
 
+            # same logic but in different direction
             if cur_node.data.moveRight():
                 temp = cur_node.data.getBoard().copy()
                 cur_node.data.moveLeft()
                 if temp not in visited:
-                    q.append(Node(PuzzelGame(temp),cur_node, cur_node.depth+1, cur_node.cost+1))
+                    q.append(Node(PuzzelGame(temp), cur_node, cur_node.depth+1))
+                    print("NEXT LEVEL")
+                    print("DEPTH: ", q[-1].depth)
+                    q[-1].data.printGame()
                     if goalCheck.goalChecker(temp):
                         return q.pop()
             
@@ -90,67 +149,67 @@ class PuzzelGame:
             if(self.puzzelBoard[i] == 0):
                 self.zeroPos = i
 
-    """need to use mod instead fixed number since we need to make the game interchangeble to different size"""
+    """need to use mod instead fixed number since we need to make the game interchangeble to different size-----------------------------------------come back later"""
     def moveUp(self):
+        # 0 cant not be at the first row
         if self.zeroPos != 0 and self.zeroPos != 1 and self.zeroPos != 2:
+            # basic swap
             temp = self.puzzelBoard[self.zeroPos - 3]
             self.puzzelBoard[self.zeroPos - 3] = self.puzzelBoard[self.zeroPos]
             self.puzzelBoard[self.zeroPos] = temp
             self.zeroPos = self.zeroPos - 3
-            self.printGame()
             return True
         else:
             return False
             
     
     def moveDown(self):
+        # 0 cant not be at the last row
         if self.zeroPos != 6 and self.zeroPos != 7 and self.zeroPos != 8:
             temp = self.puzzelBoard[self.zeroPos + 3]
             self.puzzelBoard[self.zeroPos + 3] = self.puzzelBoard[self.zeroPos]
             self.puzzelBoard[self.zeroPos] = temp
             self.zeroPos = self.zeroPos + 3
-            self.printGame()
             return True
         else:
             return False
+
     def moveLeft(self):
+        # 0 cant not be at the first column
         if self.zeroPos != 0 and self.zeroPos != 3 and self.zeroPos != 6:
             temp = self.puzzelBoard[self.zeroPos - 1]
             self.puzzelBoard[self.zeroPos - 1] = self.puzzelBoard[self.zeroPos]
             self.puzzelBoard[self.zeroPos] = temp
             self.zeroPos = self.zeroPos - 1
-            self.printGame()
             return True
         else:
             return False
 
     def moveRight(self):
+        # 0 cant not be at the last column
         if self.zeroPos != 2 and self.zeroPos != 5 and self.zeroPos != 8:
             temp = self.puzzelBoard[self.zeroPos + 1]
             self.puzzelBoard[self.zeroPos + 1] = self.puzzelBoard[self.zeroPos]
             self.puzzelBoard[self.zeroPos] = temp
             self.zeroPos = self.zeroPos + 1
-            self.printGame()
             return True
         else:   
             return False
+
+
 
 class GoalCheck:
     def __init__(self) -> None:
         pass
 
+    # come back later------------------------------------------------------------------------------------------------------
     def goalChecker(self, puzzel):
         for i in range(len(puzzel)):
             if (i == 8 and puzzel[i] != 0):
                 return False
             if(i != 8 and puzzel[i] != i+1):
                 return False
-        """need to find the best way to print the puzzel, right now is redondant"""
-        for i in range(len(puzzel)):
-            print(puzzel[i], end = " ")
-            if(i == 2 or i == 5 or i == 8):
-                print("\n")
-        
+        print("GOAL STATE FOUND!!!")
         return True
 
 class PuzzelGameIntro:
@@ -191,13 +250,12 @@ class PuzzelGameIntro:
 def main():
     start = PuzzelGameIntro()
 
-    if start.intro():
-        print("Goal Reached")
+    finalNode = start.intro()
+    print("Goal Reached")
 
-    # game.printGame()
-    # game.moveDown()
-    # game.printGame()
-    # game.moveRight()
-    # game.printGame()
+    tail = Node(finalNode.data, finalNode.parent, finalNode.depth)
+    while tail:
+        tail.data.printPath(tail.parent)
+        tail = tail.parent
 
 main()
